23e5fbfe9706fc2f2cc4c93c4245498c0616a91a
Warren Leyes <2774431+WLeyes@users.noreply.github.com>
2018-08-22 15:13:13 -0700

    Added header & footer

--- src/Components/Layout/Footer.js
+++ src/Components/Layout/Footer.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react';
-
-class Footer extends Component {
-  render() {
-    return (
-      <div>
-        Footer
+import React from 'react';
+import { CityLogo } from '../UI/Icons';
+const Footer = () => {
+  return (
+    <footer className="bck_blue">
+      <div className="footer_logo">
+        <CityLogo
+          link={true}
+          linkTo="/"
+          width="70px"
+          height="70px"
+        />
       </div>
-    );
-  }
-}
+      <div className="footer_discl">
+        Manchester City 2018. All Rights Reserved
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import './Resources/css/app.css';
 import { BrowserRouter } from 'react-router-dom';
-import Routes from './routes';
+import Routes from './Components/routes';
 
 const App = () => {
   return (
