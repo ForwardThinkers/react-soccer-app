b743396db922615b6737e343ae7c1ad41181e907
Warren Leyes <2774431+WLeyes@users.noreply.github.com>
2018-08-24 10:51:37 -0700

    added players section

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Featured from './Featured';
 import Matches from './Matches';
-
+import Players from './Players';
 const Home = () => {
   return (
     <div className="bck_blue">
       <Featured/>
       <Matches/>
+      <Players/>
     </div>
   );
 };
